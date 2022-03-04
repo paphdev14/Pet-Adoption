9f05d2994740fd047ec46b61fe4360801229c4b6
Paphery St Fleur <84752837+paphdev14@users.noreply.github.com>
2022-03-04 13:21:11 -0500

    Working files commit to git & github

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from flask import Flask, render_template, request, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db,  connect_db, Pet
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///pet_adoption'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "my-secret-app"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+connect_db(app)
+
+
+@app.route('/')
+def homepage():
+    return render_template('index.html')
